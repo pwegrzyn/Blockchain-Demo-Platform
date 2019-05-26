3f95ecd63ff299874dff27418183120c40af1e8c
Patryk Węgrzyn <wegpat@gmail.com>
2019-05-26 21:51:57 +0200

    Removed devtools

--- src/main/typescript/main.ts
+++ src/main/typescript/main.ts
@@ -13,8 +13,6 @@ function createWindow() {
     // and load the index.html of the app.
     mainWindow.loadFile(path.join(__dirname, "../src/main/resources/templates/index.html"));
 
-    // Open the DevTools.
-    mainWindow.webContents.openDevTools();
 
     // Emitted when the window is closed.
     mainWindow.on("closed", () => {
