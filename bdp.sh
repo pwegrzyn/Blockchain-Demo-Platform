2260058afd3d8518f4811884e6e6b7a9be38b032
Patryk Węgrzyn <wegpat@gmail.com>
2019-05-27 18:52:42 +0200

    Add launch script stub

---
+++ bdp.sh
@@ -0,0 +1,76 @@
+#!/bin/sh
+
+usage () {
+    echo "Blockchain Demo Platform (BDP) - simple cryptocurrency with visualization"
+    echo "usage: bdp [--version] [--help] <command> [<args>]"
+    echo "   "
+    echo "Available BDP commands:"
+    echo "  update          Update to the newest version"
+    echo "  init            Initialize the application (downloading dependencies, building etc"
+    echo "  run             Run the BDP client"
+    echo "  visualize       Run the visualization module"
+}
+
+function display_version () {
+    echo "BDP version 0.1.0"
+}
+
+function parse_args () {
+  # positional args
+  args=()
+
+  # named args
+  while [ "$1" != "" ]; do
+      case "$1" in
+          -v | --version )              display_version;         exit;;
+          -h | --help )                 usage;                   exit;; # quit and show usage
+          * )                           args+=("$1")             # if no match, add it to the positional args
+      esac
+      shift # move to next kv pair
+  done
+
+  # restore positional args
+  set -- "${args[@]}"
+
+  if [[ -z "${args[0]}" ]]; then
+    echo "Need to specify the command!"
+    usage
+    exit;
+  fi
+
+  # set positionals to vars
+  command="${args[0]}"
+
+  case command in
+    run )       run; exit;;
+    update )    update; exit;;
+    init )      init; exit;;
+    visualize ) visualize; exit;;
+    * )         echo "Unknown command!"
+  esac
+}
+
+function update () {
+    echo "Updating..."
+    git pull origin master
+    echo "Done."
+}
+
+function init () {
+    echo "Initializing..."
+    ./gradlew build
+    npm install
+    echo "Done."
+}
+
+function visualize () {
+    echo "Starting the visualization module..."
+    npm start
+}
+
+function run () {
+    echo "Starting the BDP client..."
+    ./gradlew run
+}
+
+parse_args "$@";
--- src/main/typescript/main.ts
+++ src/main/typescript/main.ts
@@ -13,13 +13,13 @@ function createWindow() {
     // and load the index.html of the app.
     mainWindow.loadFile(path.join(__dirname, "../src/main/resources/templates/index.html"));
 
-
     // Emitted when the window is closed.
     mainWindow.on("closed", () => {
         // Dereference the window object, usually you would store windows
         // in an array if your app supports multi windows, this is the time
         // when you should delete the corresponding element.
         mainWindow = null;
+        app.quit();
     });
 }
 
@@ -27,15 +27,6 @@ function createWindow() {
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.on("ready", createWindow);
-
-// Quit when all windows are closed.
-app.on("window-all-closed", () => {
-    // On OS X it is common for applications and their menu bar
-    // to stay active until the user quits explicitly with Cmd + Q
-    if (process.platform !== "darwin") {
-        app.quit();
-    }
-});
 
 app.on("activate", () => {
     // On OS X it"s common to re-create a window in the app when the
