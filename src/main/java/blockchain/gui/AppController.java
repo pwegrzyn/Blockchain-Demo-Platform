71697e2d4d8a22d9e8f6beda9484198689b66de2
gfrejek <gfrejek@gmail.com>
2019-06-08 23:23:48 +0200

    Add blockchain view, bind JavaFX controls in controller

--- src/main/java/blockchain/gui/AppController.java
+++ src/main/java/blockchain/gui/AppController.java
@@ -1,6 +1,7 @@
 package blockchain.gui;
 
 import blockchain.model.Block;
+import blockchain.model.Transaction;
 import blockchain.net.WalletNode;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
@@ -8,10 +9,7 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.control.MenuItem;
-import javafx.scene.control.TreeItem;
-import javafx.scene.control.TreeView;
+import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
@@ -25,6 +23,40 @@ public class AppController {
     private Scene primaryScene;
     private WalletNode node;
     private String currentTheme = "assets/css/defaulttheme.css";
+
+    @FXML
+    private ListView<Block> blockListView;
+
+    @FXML
+    private ListView<Transaction> txListView;
+
+    @FXML
+    private Label blockIndexLabel;
+
+    @FXML
+    private Label blockHashLabel;
+
+    @FXML
+    private Label txCountLabel;
+
+    @FXML
+    private Label prevBlockHashLabel;
+
+    @FXML
+    private Label nonceLabel;
+
+    @FXML
+    private Label timestampLabel;
+
+    @FXML
+    private Label txIndexLabel;
+
+    @FXML
+    private Label txHashLabel;
+
+    @FXML
+    private Label txTypeLabel;
+
     // Possibly can add new themes for javaFX here
     private List<String> themesList = new LinkedList<String>(){
         {
@@ -32,11 +64,20 @@ public class AppController {
         }
     };
 
+    private void test(){
+        this.blockHashLabel.setText("HASH123");
+        this.blockIndexLabel.setText("5");
+        this.prevBlockHashLabel.setText("HASH122");
+        this.timestampLabel.setText("01:25:24");
+        this.nonceLabel.setText("23");
+    }
+
     public void setPrimaryStageElements(Stage primaryStage, Scene primaryScene) {
         this.primaryStage = primaryStage;
         this.primaryScene = primaryScene;
         this.primaryScene.getStylesheets().add(this.currentTheme);
         primaryStage.setTitle("Blockchain Demo Platform");
+        test();
     }
 
     public void backToMainView() {
