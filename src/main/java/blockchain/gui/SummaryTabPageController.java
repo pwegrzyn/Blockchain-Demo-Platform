c928523471e66919e0df4a710b1ba342600425df
gfrejek <gfrejek@gmail.com>
2019-06-10 22:08:33 +0200

    Add summary tab stub.

--- src/main/java/blockchain/gui/SummaryTabPageController.java
+++ src/main/java/blockchain/gui/SummaryTabPageController.java
@@ -1,4 +1,98 @@
 package blockchain.gui;
 
+import blockchain.config.Configuration;
+import blockchain.model.Block;
+import blockchain.model.Transaction;
+import blockchain.model.TransactionInput;
+import blockchain.model.TransactionOutput;
+import blockchain.net.Node;
+import blockchain.net.WalletNode;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+import java.util.*;
+import java.util.function.Function;
+
 public class SummaryTabPageController {
+
+    @FXML private ListView usersListView;
+    @FXML private Label nodeTypeLabel;
+    @FXML private Label userCountLabel;
+    @FXML private Label blockCountLabel;
+    @FXML private Label transactionCountLabel;
+    @FXML private Label currencyAmountLabel;
+    @FXML private TextField publicKeyTextField;
+    @FXML private TextField privateKeyTextField;
+
+    private Node node;
+    private Configuration config;
+
+    public void setNode(Node node){
+        this.node = node;
+    }
+
+    public void init(){
+        config = Configuration.getInstance();
+        this.publicKeyTextField.setText(config.getPublicKey());
+        this.privateKeyTextField.setText(config.getPrivateKey());
+        this.nodeTypeLabel.setText(node instanceof WalletNode ? "Wallet node" : "Full node");
+        this.userCountLabel.setText(calculateUserCount().toString());
+        this.blockCountLabel.setText(calculateBlockCount().toString());
+        this.transactionCountLabel.setText(calculateTransactionCount().toString());
+        this.currencyAmountLabel.setText(calculateCurrencyAmount().toString());
+    }
+
+    private Integer calculateUserCount(){
+        Integer users = 0;
+        Set<String> userSet = new HashSet<>();
+
+        for(Block block : node.getBlockchain().getBlockList()){
+            for(Transaction transaction : block.getTransactions()){
+                for(TransactionOutput transactionOutput : transaction.getOutputs()){
+                    String newUser = transactionOutput.getReceiverAddress();
+
+                    if(!userSet.contains(newUser)){
+                        userSet.add(newUser);
+                        users++;
+                    }
+                }
+            }
+        }
+        return users;
+    }
+
+    private Integer calculateBlockCount(){
+        return node.getBlockchain().getBlockList().size();
+    }
+
+    private Integer calculateTransactionCount(){
+        Integer transactions = 0;
+
+        for(Block block : node.getBlockchain().getBlockList()){
+            transactions += block.getTransactions().size();
+        }
+
+        return transactions;
+    }
+
+    private Double calculateCurrencyAmount(){
+        Map<String, TransactionOutput> unusedTransactions = new HashMap<>();
+        List<String> usedTxHashes = new LinkedList<>();
+
+        for(Block block : node.getBlockchain().getBlockList()){
+            for(Transaction transaction : block.getTransactions()){
+
+                for(TransactionOutput transactionOutput : transaction.getOutputs()){
+                    String newUser = transactionOutput.getReceiverAddress();
+
+
+                }
+            }
+        }
+
+        return unusedTransactions.values().stream().mapToDouble(TransactionOutput::getAmount).sum();
+    }
+
 }
