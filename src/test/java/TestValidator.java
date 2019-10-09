65a2c8628f1a169090f354d6726ab5df82af496e
Patryk Węgrzyn <wegpat@gmail.com>
2019-10-09 18:36:30 +0200

    Add tests to travis

---
+++ src/test/java/TestValidator.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class TestValidator {
+
+    @Test
+    public void testTest() throws Exception {
+        assertTrue(true);
+    }
+
+}
