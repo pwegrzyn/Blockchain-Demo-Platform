a8d82153357363a53c58bfe3625d91bf68978135
Grzegorz Frejek <gfrejek@gmail.com>
2019-11-06 17:46:48 +0100

    Change unconfirmed transactions tab

--- src/main/java/blockchain/gui/TransactionsTabPageController.java
+++ src/main/java/blockchain/gui/TransactionsTabPageController.java
@@ -1,23 +1,13 @@
 package blockchain.gui;
 
-import blockchain.model.Blockchain;
 import blockchain.model.SynchronizedBlockchainWrapper;
-import blockchain.model.Transaction;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
-import javafx.collections.FXCollections;
-import javafx.fxml.FXML;
-import javafx.scene.control.ListView;
 import javafx.util.Duration;
-
-import java.util.stream.Collectors;
 
 public class TransactionsTabPageController {
 
     private final int refreshTimeInSeconds = 1;
-
-    @FXML
-    private ListView<String> txList;
 
     void init() {
 
@@ -39,31 +29,6 @@ public class TransactionsTabPageController {
     }
 
     private void updateListView() {
-        this.txList.setItems(FXCollections.observableArrayList(
-                SynchronizedBlockchainWrapper.useBlockchain(Blockchain::getUnconfirmedTransactions)
-                        .stream()
-                        .map(this::generateTxInfo)
-                        .collect(Collectors.toList())));
-    }
-
-    private String generateTxInfo(Transaction tx) {
-        StringBuilder result = new StringBuilder("");
-
-        result.append("FROM: ");
-        String txCreator = tx.getCreatorAddr();
-        result.append(txCreator, 0, 5).append("...").append(txCreator.substring(txCreator.length() - 5)).append("   |   ");
-
-        result.append("TO: ");
-        String toAddr = tx.getOutputs().get(0).getReceiverAddress();
-        result.append(toAddr, 0, 5).append("...").append(toAddr.substring(toAddr.length() - 5)).append("   |   ");
-
-        result.append("AMOUNT: ");
-        result.append(tx.getOutputs().get(0).getAmount()).append("    |   ");
-
-        result.append("ID: ");
-        result.append(tx.getId());
-
-        return result.toString();
     }
 
 }
